7f380b3b1401dd8a889ff0045da6f4302d8e8224
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2016-01-04 02:03:53 -0600

    Test local state via subscribe.

--- test/index.js
+++ test/index.js
@@ -1,44 +1,47 @@
 /* eslint-env mocha */
 // import { it, before, after, beforeEach, afterEach } from 'arrow-mocha/es5'
-import { it } from 'arrow-mocha/es5'
+import { it, beforeEach } from 'arrow-mocha/es5'
 
 import Firedux from '../src'
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import assert from 'assert'
 
-const firedux = new Firedux({
-  url: 'https://redux-firebase.firebaseio.com/'
-})
-
-const appReducer = (state = {}, action) => {
-  return state
-}
-
-const reducer = combineReducers({
-  app: appReducer,
-  firedux: firedux.reducer
-})
-
-const createStoreWithMiddleware = applyMiddleware(
-  thunk
-)(createStore)
-
-const store = createStoreWithMiddleware(reducer)
-
-store.subscribe(() => {
-  console.log('STATE:', JSON.stringify(store.getState(), null, '  '))
-})
-
 describe('test', t => {
+
+  let firedux
+  let reducer
+  let store
+
+  beforeEach(t => {
+    firedux = new Firedux({
+      url: 'https://redux-firebase.firebaseio.com/'
+    })
+
+    reducer = combineReducers({
+      app: (s = {}) => s,
+      firedux: firedux.reducer
+    })
+
+    store = applyMiddleware(
+      thunk
+    )(createStore)(reducer)
+  })
+
   it('should', (t, done) => {
     t.timeout(10000)
+
+    store.subscribe(() => {
+      const state = store.getState()
+      console.log('STATE:', JSON.stringify(state, null, '  '))
+      assert.equal(state.firedux.data.test, true)
+    })
+
     firedux.set(store.dispatch, 'test', true)
     .then(() => {
       return firedux.get(store.dispatch, 'test')
     }, done)
     .then((result) => {
-      console.log('DONE')
       assert.equal(result.snapshot.val(), true)
       done()
     }, done)
