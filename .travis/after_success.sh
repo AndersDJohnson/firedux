fd425f8b041bbe45139910beaa5ea9e74ebc4bc3
Anders D. Johnson <adjohnson916@users.noreply.github.com>
2016-02-03 20:34:16 -0600

    Travis github user.

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 npm run verb
+
+git config --global user.name "Travis CI"
+git config --global user.email "adjohnson916@users.noreply.github.com"
+
 if [ $? -ne 0 ]; then exit 1; fi
 git add README.md
 git commit -m "Build README.md."
