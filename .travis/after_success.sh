ed78a232e3b2c4fb175438865038a9e9a4f0ea8f
Anders D. Johnson <adjohnson916@users.noreply.github.com>
2016-02-03 21:28:50 -0600

    Travis github not quiet.

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -9,5 +9,5 @@ if [ $? -ne 0 ]; then exit 1; fi
 git add README.md
 git commit -m "Build README.md."
 if [ $? -ne 0 ]; then exit 1; fi
-git push -q "https://${GITHUB_TOKEN}@github.com/adjohnson916/firedux.git"> /dev/null 2>&1
+git push "https://${GITHUB_TOKEN}@github.com/adjohnson916/firedux.git"
 
