65a5a0e054c532a2f807d1e4633d65d55aa296ae
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2016-02-09 18:32:43 -0600

    Travis github changes.

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
-npm run verb
-
 [ "${TRAVIS_PULL_REQUEST}" = "true" ] && exit 1
+
+echo "Not a pull request..."
+echo "Setting git user..."
 
 git config user.name "Travis CI"
 git config user.email "AndersDJohnson@users.noreply.github.com"
 
-[ $? -ne 0 ] && exit 1
-git add README.md
-git commit -m "Build README.md."
-[ $? -ne 0 ] && exit 1
-git push "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" $TRAVIS_BRANCH
+echo "Running verb..."
 
+npm run verb
+
+[ $? -ne 0 ] && exit 1
+
+echo "Verb complete."
+echo "Git operations..."
+
+echo "Git checkout... branch=$TRAVIS_BRANCH"
+git checkout $TRAVIS_BRANCH
+
+echo "Git add..."
+git add README.md
+[ $? -ne 0 ] && exit 1
+echo "Git commit..."
+git commit -m "Build README.md from Travis."
+[ $? -ne 0 ] && exit 1
+echo "Git push..."
+git push "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" $TRAVIS_BRANCH
