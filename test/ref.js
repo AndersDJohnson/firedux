bbb87fd5710b67466061cb373c672ec11c6b9e78
Anders D. Johnson <adjohnson916@users.noreply.github.com>
2016-08-06 20:55:56 -0500

    test: ensure url from ref

--- test/ref.js
+++ test/ref.js
@@ -3,16 +3,20 @@
 import firebase from 'firebase'
 
 let ref
+let url
 if (process.env.FIREBASE_VERSION === '2') {
-  ref = new firebase('https://redux-firebase.firebaseio.com/')
+  url = 'https://redux-firebase.firebaseio.com/'
+  ref = new firebase(url)
 }
 else {
+  url = 'https://firedux-3d1a8.firebaseio.com/'
   const config = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: 'firedux-3d1a8.firebaseapp.com',
-    databaseURL: 'https://firedux-3d1a8.firebaseio.com'
+    databaseURL: url
   }
   firebase.initializeApp(config)
   ref = firebase.database().ref()
 }
-module.exports = ref
+
+export { ref, url }
