5978bcb588525edd2cc5f53f623e2c9b2729b6bf
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2016-08-07 22:25:49 -0500

    chore: dist

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,10 +1,12 @@
 'use strict';
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
 var _firebase = require('firebase');
 
@@ -22,6 +24,10 @@ var _updeep = require('updeep');
 
 var _updeep2 = _interopRequireDefault(_updeep);
 
+var _googleAnalyticsJs = require('google-analytics-js');
+
+var _googleAnalyticsJs2 = _interopRequireDefault(_googleAnalyticsJs);
+
 var _debug2 = require('debug');
 
 var _debug3 = _interopRequireDefault(_debug2);
@@ -30,11 +36,15 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _typeof(obj) { return obj && typeof Symbol !== "undefined" && obj.constructor === Symbol ? "symbol" : typeof obj; }
-
 var Promise = _es6Promise2.default.Promise;
 
 var debug = (0, _debug3.default)('firedux');
+
+if (typeof window !== 'undefined') {
+  if (!(window.FIREDUX_OPTIONS && window.FIREDUX_OPTIONS.noTrack)) {
+    (0, _googleAnalyticsJs2.default)('UA-82065077-1', 'github.com', '/AndersDJohnson/firedux/src/index.js');
+  }
+}
 
 var localStorage = (typeof window === 'undefined' ? 'undefined' : _typeof(window)) === 'object' ? window.localStorage : null;
 
@@ -50,17 +60,20 @@ function urlToKeyPath(url) {
   return keyPath;
 }
 
-var Firedux = (function () {
+var Firedux = function () {
   function Firedux(options) {
     _classCallCheck(this, Firedux);
 
     var that = this;
-    this.url = options.url;
+    if (options.url) {
+      console.warn('Firedux option "url" is deprecated, use "ref" instead.');
+    }
+    this.url = options.url || options.ref.toString();
+    this.ref = options.ref || new _firebase2.default(this.url);
     if (this.url.slice(-1) !== '/') {
       this.url += '/';
     }
     this.omit = options.omit || [];
-    this.ref = new _firebase2.default(this.url);
     this.token = null;
     this.getting = {};
     this.removing = {};
@@ -69,12 +82,15 @@ var Firedux = (function () {
     this.dispatch = null;
 
     function makeFirebaseState(action, state, path, value) {
+      var merge = arguments.length <= 4 || arguments[4] === undefined ? false : arguments[4];
+
       var keyPath = urlToKeyPath(path);
       // const dataPath = 'data.' + keyPath
       var dataPath = ['data'].concat(splitUrl(path));
       // const statusPath = 'status.' + keyPath
-      debug('MAKE FIREBASE STATE FOR ACTION', action.type, 'VALUE', keyPath, value);
-      var newState = _updeep2.default.updateIn(dataPath, _updeep2.default.constant(value), state);
+      debug('MAKE FIREBASE STATE FOR ACTION', action.type, 'VALUE', keyPath, value, 'merge', merge);
+      value = merge ? value : _updeep2.default.constant(value);
+      var newState = _updeep2.default.updateIn(dataPath, value, state);
       return newState;
     }
 
@@ -106,9 +122,10 @@ var Firedux = (function () {
           case 'FIREBASE_WATCH':
             return makeFirebaseState(action, state, action.path, action.snapshot.val());
           case 'FIREBASE_SET':
-          case 'FIREBASE_UPDATE':
           case 'FIREBASE_PUSH':
             return makeFirebaseState(action, state, action.path, action.value);
+          case 'FIREBASE_UPDATE':
+            return makeFirebaseState(action, state, action.path, action.value, true);
           case 'FIREBASE_REMOVE':
             return removeFirebaseState(action, state, action.path);
           case 'FIREBASE_SET_RESPONSE':
@@ -164,16 +181,18 @@ var Firedux = (function () {
         }
 
         // listen for auth changes
-        _this.ref.onAuth(function (authData) {
-          debug('FB AUTH DATA', authData);
-          if (!authData) {
-            localStorage.removeItem('FIREBASE_TOKEN');
-            that.authData = null;
-            dispatch({ type: 'FIREBASE_LOGOUT' });
-            reject();
-          }
-          resolve(authData);
-        });
+        if (_lodash2.default.isFunction(_this.ref.onAuth)) {
+          _this.ref.onAuth(function (authData) {
+            debug('FB AUTH DATA', authData);
+            if (!authData) {
+              localStorage.removeItem('FIREBASE_TOKEN');
+              that.authData = null;
+              dispatch({ type: 'FIREBASE_LOGOUT' });
+              reject();
+            }
+            resolve(authData);
+          });
+        }
       });
     }
   }, {
@@ -357,13 +376,14 @@ var Firedux = (function () {
         _this8.removing[path] = true;
         debug('FB remove', path);
 
-        var value = undefined;
+        var value = void 0;
 
         // optimism
         dispatch({
           type: 'FIREBASE_REMOVE',
           path: path,
           // TODO: How to access state for cleaner rollback?
+          // eslint-disable-next-line no-return-assign
           setValue: function setValue(v) {
             return value = v;
           }
@@ -392,8 +412,8 @@ var Firedux = (function () {
       return new Promise(function (resolve, reject) {
         debug('FB PUSH', toPath, newValue);
 
-        var path = undefined,
-            newId = undefined;
+        var path = void 0,
+            newId = void 0;
         var ref = that.ref.child(toPath);
         var pushRef = ref.push(newValue, function (error) {
           dispatch({
@@ -408,7 +428,8 @@ var Firedux = (function () {
           if (error) reject(error);else resolve(newId);
         });
         path = pushRef.toString().replace(that.url, '');
-        newId = pushRef.key();
+        // function in firebase@2, property in firebase@3
+        newId = _lodash2.default.isFunction(pushRef, 'key') ? pushRef.key() : pushRef.key;
         if (onId) onId(newId);
 
         // optimism
@@ -426,6 +447,6 @@ var Firedux = (function () {
   }]);
 
   return Firedux;
-})();
+}();
 
 exports.default = Firedux;
