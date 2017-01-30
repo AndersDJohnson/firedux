c570559e91070035cb1658bc57467a4671e7d3ec
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2017-01-29 20:45:25 -0600

    chore: lint test

--- test/ref.js
+++ test/ref.js
@@ -1,22 +1,21 @@
 /* eslint-env mocha */
 
-import firebase from 'firebase'
+import Firebase from 'firebase'
 
 let ref
 let url
 if (process.env.FIREBASE_VERSION === '2') {
   url = 'https://redux-firebase.firebaseio.com/'
-  ref = new firebase(url)
-}
-else {
+  ref = new Firebase(url)
+} else {
   url = 'https://firedux-3d1a8.firebaseio.com/'
   const config = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: 'firedux-3d1a8.firebaseapp.com',
     databaseURL: url
   }
-  firebase.initializeApp(config)
-  ref = firebase.database().ref()
+  Firebase.initializeApp(config)
+  ref = Firebase.database().ref()
 }
 
 export { ref, url }
